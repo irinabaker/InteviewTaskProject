22abe1c1bf2861714880bf4be0c2796606f02a07
Ira Baker <49585685+irinabaker@users.noreply.github.com>
2023-07-04 17:04:37 +0200

    First commit

---
+++ src/test/java/com/ebay/tests/SearchItemTest.java
@@ -0,0 +1,31 @@
+package com.ebay.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SearchItemTest extends TestBase{
+
+    @Test
+    public void searchItem() {
+
+        app.getSelect().selectDepartment("Elektronik");
+
+        app.getSelect().selectCategoryType("1");
+
+        app.getSelect().selectCategoryType("1");
+
+        String secondItemName = app.getItem().getItemName("2");
+        System.out.println(secondItemName);
+
+        app.getItem().fillSearchBar(secondItemName);
+
+        String firstItemName = app.getItem().getFirstItemName("2");
+        System.out.println(firstItemName);
+
+       // Assert.assertEquals(firstItemName,secondItemName);
+
+        Assert.assertTrue(firstItemName.contains(secondItemName));
+
+    }
+}
+
